60884c2e90724ca4536662e698ce9028c67f289a
naemsaleem <80448271+naemsaleem@users.noreply.github.com>
2021-04-20 05:06:52 +1000

    Q2_partd2.sql

---
+++ Q2_partd3.sql
@@ -0,0 +1,2 @@
+Mysql –u awkologi –p compbiol –e 
+SELECT Distinct m.LastName, m.FirstName, e.MemberID FROM ((Entry e inner join Entry f on e.MemberID=f.MemberID) inner join Entry g on f.MemberID=g.MemberID) inner join Member m on m.MemberID=e.MemberID WHERE e.Year=2013 and f.Year=2014 and g.Year=2015;
